de087ed0b03d20c259fcedbdb09101921544ee28
Craig Melville <acekreations@gmail.com>
2018-08-04 17:13:35 -0700

    Add user input functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
   //VARIABLES
-  //Rand num that is used as the goal for the player to reach
-  var computerNum = randNum(19, 120);
-  //Rand num for each of the crystal buttons
-  var crystalNum = randNum(1, 12);
+  var userTotal = 0;
 
   //FUNCTIONS
   //Create random number with a min and max
@@ -11,11 +8,46 @@ $(document).ready(function(){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
 
-  function functionName() {
-
+  //Calculates and displays target number
+  function displayTargetNum() {
+    var targetNum = $("#targetNum");
+    var num = randNum(19, 120);
+    targetNum.val("");
+    targetNum.val(num);
+    targetNum.text(num);
   }
 
+  //Add rand num to each crystal btn as a value
+  function addCrystalBtnVal() {
+    $(".crystalBtn").each(function(){
+      $(this).val("");
+      var val = randNum(1, 12);
+      $(this).attr("value", val);
+    })
+  }
 
+  //Display user total on page
+  function displayUserTotal() {
+    $("#userTotal").text(userTotal);
+  }
+
+  function calcUserTotal(clickedBtnVal) {
+    userInput = parseInt(clickedBtnVal);
+    userTotal = userTotal + userInput;
+    displayUserTotal();
+  }
+
+  displayTargetNum();
+
+  addCrystalBtnVal();
+
+  //ACTIONS
+
+  //When crystal btn is pressed get value and add it to the user total
+  $(".crystalBtn").on("click", function() {
+    var clickedBtnVal = $(this).attr("value");
+    calcUserTotal(clickedBtnVal);
+  })
 
 
 
